f76513641c78c2d71529de4a36edee1310dcca08
Tilman Schlenker <tilman@mailbox.org>
2018-09-17 00:12:55 +0200

    nesting hooray

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,7 @@ type Msg
 
 
 init flags =
-    ( Model 1 7 [], Cmd.none )
+    ( Model 1 5 [ 5, 4 ], Cmd.none )
 
 
 update msg model =
@@ -67,13 +67,42 @@ view model =
                     )
                     { previous = Nothing, lines = [] }
 
+        items =
+            List.foldl (\c svg -> groupItems c scale svg) lines.lines model.repeats
+
+        pp =
+            Debug.log "Point1" (pointOnCircle 3 0)
+
         _ =
-            Debug.log "Points" (Debug.toString lines)
+            Debug.log "Points"
+                (Debug.toString
+                    (List.range 0 2
+                        --|> List.map toFloat
+                        |> List.map (pointOnCircle 3)
+                        |> List.map (\( x, y ) -> ( x * scale, y * scale ))
+                    )
+                )
     in
     svg [ width "500", height "500", viewBox "-105 -105 210 210" ]
-        (circle [ x "0", y "0", r "100", stroke "none", fill "none" ] []
-            :: lines.lines
-        )
+        (circle [ x "0", y "0", r "100", stroke "none", fill "none" ] [] :: items)
+
+
+groupItems : Int -> Float -> List (Svg msg) -> List (Svg msg)
+groupItems count scale items =
+    List.range 0 (count - 1)
+        --|> List.map toFloat
+        |> List.map (pointOnCircle count)
+        |> List.map (\( x, y ) -> ( x * scale, y * scale ))
+        |> List.map (\( x, y ) -> g [ transform ("scale(0.5 0.5) translate(" ++ String.fromFloat x ++ "," ++ String.fromFloat y ++ ")") ] items)
+
+
+pointOnCircle : Int -> Int -> ( Float, Float )
+pointOnCircle total index =
+    let
+        radius =
+            2 * pi / toFloat total * toFloat index - pi / 2
+    in
+    Debug.log "Point" ( cos radius, sin radius )
 
 
 drawLine x1_ y1_ x2_ y2_ =
